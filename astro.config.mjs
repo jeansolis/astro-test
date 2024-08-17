6a5509aa2cd046a35cc324af5c97db282add7c30
jeansolis <jeansolisa@gmail.com>
2024-08-17 22:42:05 +0200

    Add git hub pages workflow to test deployment there

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
+// export default defineConfig({
+//   output: "server",
+//   adapter: netlify({
+//     edgeMiddleware: true,
+//   })
+// });
+
 export default defineConfig({
-  output: "server",
-  adapter: netlify({
-    edgeMiddleware: true,
+    site: 'https://jeansolis.github.io',
+    base: 'astro-test',
   })
-});
